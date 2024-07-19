f0efce669a2582664ac37c193a1e4da38264b0d2
Rich Fiorella <rfiorella@lanl.gov>
2024-07-19 17:03:20 -0600

    initial update to fit my workflows

--- Brewfile
+++ Brewfile
@@ -1,64 +1,45 @@
 # Taps
-tap 'homebrew/cask-fonts'
-tap 'homebrew/cask-versions'
+#tap 'homebrew/cask-fonts'
+#tap 'homebrew/cask-versions'
 tap 'homebrew/bundle'
-tap 'stripe/stripe-cli'
 
-# Binaries
+# Generic 
 brew 'awscli'
 brew 'bash' # Latest Bash version
-brew 'bat' # Used for spatie/visit
 brew 'coreutils' # Those that come with macOS are outdated
-brew 'ffmpeg'
-brew 'gh'
 brew 'git'
 brew 'grep'
-brew 'httpie'
-brew 'jq' # Used for spatie/visit
-brew 'mackup'
-brew 'mas' # Mac App Store manager
-brew 'pkg-config' # https://github.com/driesvints/dotfiles/issues/20
-brew 'stripe/stripe-cli/stripe'
-brew 'stripe/stripe-mock/stripe-mock'
 brew 'svn' # Needed to install fonts
-
-# Spatie Medialibrary
-brew 'jpegoptim'
-brew 'optipng'
-brew 'pngquant'
-brew 'svgo'
-brew 'gifsicle'
+brew 'subversion'
+brew 'htop'
+brew 'wget'
+brew 'zsh-completion'
 
 # Development
-brew 'imagemagick'
-brew 'yarn'
+brew 'cmake'
+brew 'make'
+brew 'gcc'
+brew 'openmpi'
+brew 'modules'
+
+# netcdf tools:
+brew 'cdo'
+brew 'eccodes'
+brew 'hdf5' 
+brew 'netcdf'
+brew 'netcdf-fortran'
+brew 'nco'
+brew 'ncview'
 
 # Apps
-cask '1password'
-cask 'caffeine'
-cask 'discord'
-cask 'docker'
-cask 'figma'
-cask 'firefox'
-cask 'github'
-cask 'google-chrome'
-cask 'helo'
-cask 'herd'
-cask 'imageoptim'
-cask 'insomnia'
-cask 'pastebot'
-cask 'readdle-spark'
-cask 'screenflow'
-cask 'slack'
-cask 'tableplus'
-cask 'telegram-desktop'
-cask 'the-unarchiver'
-cask 'tinkerwell'
-cask 'transmit'
-cask 'tunnelbear'
-cask 'tuple'
-cask 'visual-studio-code'
-cask 'zoom'
+# cask 'docker'
+#cask 'visual-studio-code'
+cask 'iterm2'
+cask 'obsidian'
+cask 'visit'
+cask 'mactex'
+cask 'paraview'
+
 
 # Quicklook
 cask 'qlmarkdown'
@@ -73,12 +54,3 @@ cask 'font-source-code-pro'
 cask 'font-source-sans-pro'
 cask 'font-source-serif-pro'
 
-# Mac App Store
-mas 'Byword', id: 420212497
-mas 'Giphy Capture', id: 668208984
-mas 'Keynote', id: 409183694
-mas 'MyWoosh', id: 1498889644
-mas 'Numbers', id: 409203825
-mas 'Speedtest', id: 1153157709
-mas 'Spring', id: 1508706541
-mas 'Things', id: 904280696
--- fresh.sh
+++ fresh.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# set some options about whether on a work computer or at home
+Help()
+{
+	echo "Usage: $(basename $0) -h|--help --user"
+	echo "Options:"
+	echo "  -h, --help	Display this help message."
+	echo "  --user		Install assuming no sudo available - biggest change is around installing casks"
+}
+
 echo "Setting up your Mac..."
 
 # Check for Oh My Zsh and install if we don't have it
@@ -10,14 +19,19 @@ fi
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  # this needs some logic to change based on where brew is actually being installed:
+  if [ `uname -m` == "arm64" ]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else 
+    echo 'eval "$(/usr/local/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
+#rm -rf $HOME/.zshrc
+#ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
@@ -26,22 +40,32 @@ brew update
 brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
+# Install miniconda:
+if test ! $(which conda); then
+   mkdir -p ~/miniconda3
+  /bin/bash -c "$(curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-`uname -m`.sh -o ~/miniconda3/miniconda.sh)"
+  /bin/bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+  rm -rf ~/miniconda3/miniconda.sh
+  # apply initialization script:
+  ~/miniconda3/bin/conda init zsh
+fi
+
 # Set default MySQL root password and auth type
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+#mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Create a projects directories
-mkdir $HOME/Code
-mkdir $HOME/Herd
+#mkdir $HOME/Code
+#mkdir $HOME/Herd
 
 # Create Code subdirectories
-mkdir $HOME/Code/blade-ui-kit
-mkdir $HOME/Code/laravel
+#mkdir $HOME/Code/blade-ui-kit
+#mkdir $HOME/Code/laravel
 
 # Clone Github repositories
-./clone.sh
+#./clone.sh
 
 # Symlink the Mackup config file to the home directory
-ln -s ./.mackup.cfg $HOME/.mackup.cfg
+#ln -s ./.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
-source ./.macos
+#source ./.macos
